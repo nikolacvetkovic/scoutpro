42a4302e1f773c39d72ca25588591abf69b4641b
Nikola <nikolac@telekom.rs>
2021-08-09 19:19:50 +0200

    delete ScrapeRegExpression classes, create new model and update all references, update data sql script

---
+++ src/main/java/xyz/riocode/scoutpro/scrape/model/ScrapeSite.java
@@ -0,0 +1,30 @@
+package xyz.riocode.scoutpro.scrape.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "scrape_site")
+public class ScrapeSite implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "scrapeSite", fetch = FetchType.LAZY, orphanRemoval = true)
+    private Set<ScrapeField> scrapeFields = new HashSet<>();
+
+}
