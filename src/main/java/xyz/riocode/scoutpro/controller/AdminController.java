b5e48b47f413d1d7cf54b4cfbf67bbd3bd9e300e
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-04-12 23:46:17 +0200

    add admin pages

--- src/main/java/xyz/riocode/scoutpro/controller/AdminController.java
+++ src/main/java/xyz/riocode/scoutpro/controller/AdminController.java
@@ -1,10 +1,7 @@
 package xyz.riocode.scoutpro.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.riocode.scoutpro.scrape.service.ScrapeFieldService;
@@ -22,10 +19,20 @@ public class AdminController {
         this.scrapeFieldService = scrapeFieldService;
     }
 
-
     @GetMapping("/dashboard")
     public String showAdminDashboard(){
         return "admin/dashboard";
+    }
+
+    @GetMapping("/scrapefields")
+    public String showScrapeFields(){
+
+        return "admin/scrapeFields";
+    }
+
+    @GetMapping("/jobs")
+    public String showJobDashboard() {
+        return "admin/jobDashboard";
     }
 
     @GetMapping("/user")
@@ -38,41 +45,5 @@ public class AdminController {
     public String saveOrUpdateUser(){
 
         return "redirect:/admin/user";
-    }
-
-    @GetMapping("/regex")
-    public String showRegex(){
-
-        return "admin/regex";
-    }
-
-    @PostMapping("regex")
-    public String updateRegex(){
-
-        return "redirect:/admin/regex";
-    }
-
-    @GetMapping("/{tmUrl}/tm")
-    public ResponseEntity checkTMScrape(@PathVariable String tmUrl){
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @GetMapping("/{wsUrl}/ws")
-    public ResponseEntity checkWSScrape(@PathVariable String wsUrl){
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @GetMapping("/{pesDbUrl}/pesdb")
-    public ResponseEntity checkPesDbScrape(@PathVariable String pesDbUrl){
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @GetMapping("/{psmlUrl}/psml")
-    public ResponseEntity checkPsmlScrape(@PathVariable String psmlUrl){
-
-        return new ResponseEntity(HttpStatus.OK);
     }
 }
