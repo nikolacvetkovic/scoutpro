a6cf50db6cd8a6aa6dde289a5d7a655309af0651
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-04-18 17:45:12 +0200

    delete log interceptor

--- src/main/java/xyz/riocode/scoutpro/config/WebConfig.java
+++ src/main/java/xyz/riocode/scoutpro/config/WebConfig.java
@@ -2,9 +2,7 @@ package xyz.riocode.scoutpro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import xyz.riocode.scoutpro.interceptor.LogInterceptor;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -13,12 +11,6 @@ import javax.validation.ValidatorFactory;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
-    }
-
     @Bean
     public Validator validator(){
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
