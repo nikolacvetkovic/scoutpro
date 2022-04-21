af0f5c4102d7eaf11ad95663d327d5b94a515b44
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-04-21 22:30:19 +0200

    remove string field

--- src/main/java/xyz/riocode/scoutpro/scheduler/enums/JobStatus.java
+++ src/main/java/xyz/riocode/scoutpro/scheduler/enums/JobStatus.java
@@ -2,14 +2,8 @@ package xyz.riocode.scoutpro.scheduler.enums;
 
 public enum JobStatus {
 
-    CREATED("CREATED"),
-    SCHEDULED("SCHEDULED"),
-    PAUSED("PAUSED");
-
-    private String statusValue;
-
-    JobStatus(String statusValue) {
-        this.statusValue = statusValue;
-    }
+    CREATED,
+    SCHEDULED,
+    PAUSED;
 
 }
