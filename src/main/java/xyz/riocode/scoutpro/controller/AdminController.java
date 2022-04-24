f18f5627fb6323abcc6f64528c86af91229b0bc1
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-04-25 01:06:52 +0200

    create and implement admin dashboard page

--- src/main/java/xyz/riocode/scoutpro/controller/AdminController.java
+++ src/main/java/xyz/riocode/scoutpro/controller/AdminController.java
@@ -5,14 +5,18 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.riocode.scoutpro.converter.JobExecutionHistoryConverter;
+import xyz.riocode.scoutpro.converter.ScrapeErrorConverter;
 import xyz.riocode.scoutpro.converter.ScrapeSiteConverter;
+import xyz.riocode.scoutpro.dto.JobExecutionHistoryDTO;
+import xyz.riocode.scoutpro.dto.ScrapeErrorDTO;
 import xyz.riocode.scoutpro.dto.ScrapeSiteDTO;
-import xyz.riocode.scoutpro.scrape.service.ScrapeFieldService;
+import xyz.riocode.scoutpro.scheduler.service.JobExecutionHistoryService;
+import xyz.riocode.scoutpro.scrape.service.ScrapeErrorService;
 import xyz.riocode.scoutpro.scrape.service.ScrapeSiteService;
 import xyz.riocode.scoutpro.security.privilege.AdminDashboardPrivilege;
 import xyz.riocode.scoutpro.security.privilege.JobReadPrivilege;
 import xyz.riocode.scoutpro.security.privilege.ScrapeSiteReadPrivilege;
-import xyz.riocode.scoutpro.service.security.AppUserService;
 
 import java.util.List;
 
@@ -20,24 +24,38 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final AppUserService userService;
     private final ScrapeSiteService scrapeSiteService;
-    private final ScrapeFieldService scrapeFieldService;
-
+    private final ScrapeErrorService scrapeErrorService;
+    private final JobExecutionHistoryService jobExecutionHistoryService;
     private final ScrapeSiteConverter scrapeSiteConverter;
+    private final ScrapeErrorConverter scrapeErrorConverter;
+    private final JobExecutionHistoryConverter jobExecutionHistoryConverter;
 
-    public AdminController(AppUserService userService, ScrapeSiteService scrapeSiteService, ScrapeFieldService scrapeFieldService, ScrapeSiteConverter scrapeSiteConverter) {
-        this.userService = userService;
+    public AdminController(ScrapeSiteService scrapeSiteService, ScrapeSiteConverter scrapeSiteConverter, ScrapeErrorService scrapeErrorService, JobExecutionHistoryService jobExecutionHistoryService, ScrapeErrorConverter scrapeErrorConverter, JobExecutionHistoryConverter jobExecutionHistoryConverter) {
         this.scrapeSiteService = scrapeSiteService;
-        this.scrapeFieldService = scrapeFieldService;
         this.scrapeSiteConverter = scrapeSiteConverter;
+        this.scrapeErrorService = scrapeErrorService;
+        this.jobExecutionHistoryService = jobExecutionHistoryService;
+        this.scrapeErrorConverter = scrapeErrorConverter;
+        this.jobExecutionHistoryConverter = jobExecutionHistoryConverter;
     }
 
     @AdminDashboardPrivilege
     @GetMapping("/dashboard")
-    public String showAdminDashboard(){
+    public String showAdminDashboard(ModelMap modelMap){
+        List<ScrapeSiteDTO> scrapeSiteDTOs = scrapeSiteConverter.scrapeSiteToScrapeSiteDTO(
+                                                                                    scrapeSiteService.getAll());
+        List<ScrapeErrorDTO> scrapeErrorsDTOs = scrapeErrorConverter.scrapeErrorToScrapeErrorDTO(
+                                                                                    scrapeErrorService.getAll());
+        List<JobExecutionHistoryDTO> jobExecutionHistoryDTOs = jobExecutionHistoryConverter.jobExecutionToJobExecutionDTO(
+                                                                                    jobExecutionHistoryService.getAll());
+
+        modelMap.addAttribute("scrapeSites", scrapeSiteDTOs);
+        modelMap.addAttribute("scrapeErrors", scrapeErrorsDTOs);
+        modelMap.addAttribute("jobExecutions", jobExecutionHistoryDTOs);
         return "admin/dashboard";
     }
+
     @ScrapeSiteReadPrivilege
     @GetMapping("/scrapefields")
     public String showScrapeFields(ModelMap modelMap){
@@ -45,6 +63,7 @@ public class AdminController {
         modelMap.addAttribute("scrapeSites", scrapeSiteDTOS);
         return "admin/scrapeFields";
     }
+
     @JobReadPrivilege
     @GetMapping("/jobs")
     public String showJobDashboard() {
