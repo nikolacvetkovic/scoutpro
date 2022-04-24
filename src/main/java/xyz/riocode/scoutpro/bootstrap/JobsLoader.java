8049842fd9cab626c2cfae2e1863545af0d08c9e
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-04-25 00:27:49 +0200

    create jobs for checking scrape fields - scrape status

--- src/main/java/xyz/riocode/scoutpro/bootstrap/JobsLoader.java
+++ src/main/java/xyz/riocode/scoutpro/bootstrap/JobsLoader.java
@@ -82,5 +82,32 @@ public class JobsLoader implements CommandLineRunner {
 //                .build();
 //
 //        jobService.createJob(psmlTransferImport);
+//
+//        JobInfo tmCoreScrapeCheck = JobInfo.builder()
+//                .jobClass("xyz.riocode.scoutpro.scheduler.job.TMCoreScrapeCheck")
+//                .jobName("TMCoreScrapeCheck")
+//                .jobGroup("Transfermarkt")
+//                .customConfigData("")
+//                .build();
+//
+//        jobService.createJob(tmCoreScrapeCheck);
+//
+//        JobInfo pesDbScrapeCheck = JobInfo.builder()
+//                .jobClass("xyz.riocode.scoutpro.scheduler.job.PesDbScrapeCheck")
+//                .jobName("PesDbScrapeCheck")
+//                .jobGroup("PesDb")
+//                .customConfigData("")
+//                .build();
+//
+//        jobService.createJob(pesDbScrapeCheck);
+//
+//        JobInfo psmlScrapeCheck = JobInfo.builder()
+//                .jobClass("xyz.riocode.scoutpro.scheduler.job.PsmlScrapeCheck")
+//                .jobName("PsmlScrapeCheck")
+//                .jobGroup("Psml")
+//                .customConfigData("")
+//                .build();
+//
+//        jobService.createJob(psmlScrapeCheck);
     }
 }
---
+++ src/main/java/xyz/riocode/scoutpro/scheduler/job/TMCoreScrapeCheck.java
@@ -0,0 +1,12 @@
+package xyz.riocode.scoutpro.scheduler.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class TMCoreScrapeCheck extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+
+    }
+}
