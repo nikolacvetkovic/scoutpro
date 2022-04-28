30f495127e346c95e50e08956a338d79abe47b05
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-04-28 19:39:06 +0200

    add method

--- src/main/java/xyz/riocode/scoutpro/converter/JobConverter.java
+++ src/main/java/xyz/riocode/scoutpro/converter/JobConverter.java
@@ -2,8 +2,10 @@ package xyz.riocode.scoutpro.converter;
 
 import org.springframework.stereotype.Component;
 import xyz.riocode.scoutpro.dto.JobDTO;
+import xyz.riocode.scoutpro.scheduler.enums.JobStatus;
 import xyz.riocode.scoutpro.scheduler.model.JobInfo;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
@@ -27,6 +29,22 @@ public class JobConverter {
                 .repeatCount(job.getRepeatCount())
                 .repeatInterval(job.getRepeatIntervalInSeconds())
                 .customConfig(job.getCustomConfigData())
+                .build();
+    }
+
+    public JobInfo jobDTOToJobInfo(JobDTO job) {
+        return JobInfo.builder()
+                .id(Long.valueOf(job.getId()))
+                .jobName(job.getName())
+                .jobGroup(job.getGroup())
+                .jobStatus(JobStatus.valueOf(job.getStatus()))
+                .startTime((job.getStartTime()!=null && !job.getStartTime().isEmpty()) ? LocalDateTime.parse(job.getStartTime(), dateTimeFormatter):null)
+                .endTime((job.getEndTime()!=null && !job.getEndTime().isEmpty()) ? LocalDateTime.parse(job.getEndTime(), dateTimeFormatter):null)
+                .cronJob(job.isCronJob())
+                .cronExpression(job.getCronExp())
+                .repeatCount(job.getRepeatCount())
+                .repeatIntervalInSeconds(job.getRepeatInterval())
+                .customConfigData(job.getCustomConfig())
                 .build();
     }
 
