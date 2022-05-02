8f347afaf8d3aa78ec410fd202c2691f408ebce6
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-05-02 15:59:12 +0200

    add method

--- src/main/java/xyz/riocode/scoutpro/scrape/repository/ScrapeSiteRepository.java
+++ src/main/java/xyz/riocode/scoutpro/scrape/repository/ScrapeSiteRepository.java
@@ -1,10 +1,17 @@
 package xyz.riocode.scoutpro.scrape.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import xyz.riocode.scoutpro.scrape.model.ScrapeSite;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ScrapeSiteRepository extends JpaRepository<ScrapeSite, Long> {
 
     ScrapeSite findByHostname(String hostname);
+    @Query(value = "SELECT s FROM ScrapeSite s " +
+            "WHERE s.lastChecked < :lastChecked")
+    List<ScrapeSite> findByLastCheckedBefore(LocalDateTime lastChecked);
 
 }
