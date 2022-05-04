79f5a7217088e149dfd8dfed09f733fdafe9cdfc
cvelenidza <nikola.cvetkovic91@gmail.com>
2022-05-05 00:30:30 +0200

    add sorting for scrapeErrors and jobExecutions, fix tables height in admin dash

--- src/main/java/xyz/riocode/scoutpro/converter/JobExecutionHistoryConverter.java
+++ src/main/java/xyz/riocode/scoutpro/converter/JobExecutionHistoryConverter.java
@@ -5,6 +5,7 @@ import xyz.riocode.scoutpro.dto.JobExecutionHistoryDTO;
 import xyz.riocode.scoutpro.scheduler.model.JobExecutionHistory;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -15,17 +16,19 @@ public class JobExecutionHistoryConverter {
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss").withLocale(Locale.US);
 
     public List<JobExecutionHistoryDTO> jobExecutionToJobExecutionDTO(List<JobExecutionHistory> jobExecutionHistories) {
-        return jobExecutionHistories.stream().map(jobExecutionHistory -> {
-            return JobExecutionHistoryDTO.builder()
-                    .jobName(jobExecutionHistory.getJobInfo().getJobName())
-                    .jobStatus(jobExecutionHistory.getJobInfo().getJobStatus().name())
-                    .startTime(jobExecutionHistory.getStartTime().format(dateTimeFormatter))
-                    .endTime(jobExecutionHistory.getEndTime().format(dateTimeFormatter))
-                    .status(jobExecutionHistory.getStatus().name())
-                    .playersProcessed(jobExecutionHistory.getPlayersProcessed().toString())
-                    .playersWithError(jobExecutionHistory.getPlayersWithError().toString())
-                    .errorStackTrace(jobExecutionHistory.getErrorStackTrace())
-                    .build();
+        return jobExecutionHistories.stream()
+                .sorted(Comparator.comparing(JobExecutionHistory::getEndTime).reversed())
+                .map(jobExecutionHistory -> {
+                    return JobExecutionHistoryDTO.builder()
+                            .jobName(jobExecutionHistory.getJobInfo().getJobName())
+                            .jobStatus(jobExecutionHistory.getJobInfo().getJobStatus().name())
+                            .startTime(jobExecutionHistory.getStartTime().format(dateTimeFormatter))
+                            .endTime(jobExecutionHistory.getEndTime().format(dateTimeFormatter))
+                            .status(jobExecutionHistory.getStatus().name())
+                            .playersProcessed(jobExecutionHistory.getPlayersProcessed().toString())
+                            .playersWithError(jobExecutionHistory.getPlayersWithError().toString())
+                            .errorStackTrace(jobExecutionHistory.getErrorStackTrace())
+                            .build();
         }).collect(Collectors.toList());
     }
 
--- src/main/java/xyz/riocode/scoutpro/converter/ScrapeErrorConverter.java
+++ src/main/java/xyz/riocode/scoutpro/converter/ScrapeErrorConverter.java
@@ -5,6 +5,7 @@ import xyz.riocode.scoutpro.dto.ScrapeErrorDTO;
 import xyz.riocode.scoutpro.scrape.model.ScrapeError;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -15,12 +16,15 @@ public class ScrapeErrorConverter {
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss").withLocale(Locale.US);
 
     public List<ScrapeErrorDTO> scrapeErrorToScrapeErrorDTO(List<ScrapeError> scrapeErrors) {
-        return scrapeErrors.stream().map(scrapeError -> {
-            return ScrapeErrorDTO.builder()
-                    .time(scrapeError.getScrapeTime().format(dateTimeFormatter))
-                    .jobName(scrapeError.getJobInfo()!=null?scrapeError.getJobInfo().getJobName():"")
-                    .playerName(scrapeError.getPlayer()!=null?scrapeError.getPlayer().getName():"")
-                    .build();
+        return scrapeErrors.stream()
+                .sorted(Comparator.comparing(ScrapeError::getScrapeTime).reversed())
+                .map(scrapeError -> {
+                    return ScrapeErrorDTO.builder()
+                            .time(scrapeError.getScrapeTime().format(dateTimeFormatter))
+                            .jobName(scrapeError.getJobInfo()!=null?scrapeError.getJobInfo().getJobName():"")
+                            .playerName(scrapeError.getPlayer()!=null?scrapeError.getPlayer().getName():"")
+                            .stackTrace(scrapeError.getStackTrace())
+                            .build();
         }).collect(Collectors.toList());
     }
 
